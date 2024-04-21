f9254b2ddcc837c6ccccb798e1b8095ac6b221d6
unknown <ijsathi2001rs@gmail.com>
2024-04-21 15:03:41 +0600

    add FoodItem class

--- users.py
+++ users.py
@@ -23,10 +23,17 @@ class Admin(User):
     def view_employee(self, restaurent):
         restaurent.view_employee()
 
+    def add_new_item(self, restaurent, item):
+        restaurent.menu.add_menu_item(item)
+
+    def remove_item(self, restaurent, item):
+        restaurent.menu.remove_item(item)
+
 class Restaurent:
     def __init__(self, name):
         self.name = name
         self.employees = [] # Its our Database
+        self.menu = FoodItem()
 
     def add_employee(self, employee):
         self.employees.append(employee)
@@ -56,8 +63,24 @@ class Menu:
             print("Item Deleted!!")
         else:
             print("Item Not Found!!")
+    
+    def show_menu(self):
+        print("****Menu****")
+        print("Name\tPrice\tQuantity")
+        for item in self.items:
+            print(f"{item.name}\t{item.price}\t{item.quantity}")
 
+class FoodItem:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
-ad = Admin('karim', 1234, 'k@gmail.com', 'dhaka')
-ad.add_employee('hahim', 9874, 'h@gmail.com', 'dhaka', 34, 'IT', 34000)
-ad.view_employee()
+mn = Menu()
+item = FoodItem("Pizza", 299, 15)
+mn.add_menu_item(item)
+mn.show_menu()
+
+# ad = Admin('karim', 1234, 'k@gmail.com', 'dhaka')
+# ad.add_employee('hahim', 9874, 'h@gmail.com', 'dhaka', 34, 'IT', 34000)
+# ad.view_employee()
